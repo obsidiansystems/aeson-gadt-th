509dd9997e87f7f555585126ba329859eadb4d8e
Ali Abrar <aliabrar@gmail.com>
2019-01-23 17:56:27 -0500

    WIP: Start work on higher-kinded GADTs

--- src/Data/Aeson/GADT/TH.hs
+++ src/Data/Aeson/GADT/TH.hs
@@ -15,12 +15,16 @@
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module Data.Aeson.GADT.TH where
+module Data.Aeson.GADT.TH (deriveJSONGADT, deriveToJSONGADT, deriveFromJSONGADT) where
 
 import Control.Monad
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Writer
 import Data.Aeson
 import Data.Dependent.Sum
 import Data.Functor.Classes
+import Data.List
+import Data.Maybe
 import Data.Some (Some (..))
 import Language.Haskell.TH
 
@@ -56,20 +60,31 @@ conArity c = case c of
   GadtC _ ts _ -> length ts
   RecGadtC _ ts _ -> length ts
 
-{-# DEPRECATED deriveGADTInstances "Use deriveJSONGADT instead" #-}
-deriveGADTInstances :: Name -> DecsQ
-deriveGADTInstances = deriveJSONGADT
-
 deriveToJSONGADT :: Name -> DecsQ
 deriveToJSONGADT n = do
   x <- reify n
   let cons = case x of
        TyConI d -> decCons d
        _ -> error "undefined"
+  arity <- tyConArity n
+  tyVars <- replicateM arity (newName "a")
+  let n' = foldr (\v x -> AppT x (VarT v)) (ConT n) tyVars
   [d|
-    instance ToJSON ($(conT n) a) where
+    instance ToJSON $(pure n') where
       toJSON r = $(caseE [|r|] $ map conMatchesToJSON cons)
     |]
+
+{-
+  tyVars <- replicateM (arity - 1) (newName "a")
+  let n' = foldr (\v x -> AppT x (VarT v)) (ConT n) tyVars
+  [d| instance ArgDict $(pure n') where
+        type ConstraintsFor  $(pure n') $(varT c) = $(pure constraints)
+        type ConstraintsFor' $(pure n') $(varT c) $(varT g) = $(pure constraints')
+        argDict = $(LamCaseE <$> matches n 'argDict)
+        argDict' = $(LamCaseE <$> matches n 'argDict')
+|]
+-}
+
 
 deriveFromJSONGADT :: Name -> DecsQ
 deriveFromJSONGADT n = do
@@ -78,38 +93,19 @@ deriveFromJSONGADT n = do
        TyConI d -> decCons d
        _ -> error "undefined"
   let wild = match wildP (normalB [|fail "deriveFromJSONGADT: Supposedly-complete GADT pattern match fell through in generated code. This shouldn't happen."|]) []
+  arity <- tyConArity n
+  tyVars <- replicateM (arity - 1) (newName "a")
+  let n' = foldr (\v x -> AppT x (VarT v)) (ConT n) tyVars
+  (matches, typs) <- runWriterT $ mapM (conMatchesParseJSON [|v'|]) cons
+  let nubbedTypes = map head . group . sort $ typs -- This 'head' is safe because 'group' returns a list of non-empty lists
+      constraints = foldl AppT (TupleT (length nubbedTypes)) $
+        map (AppT (ConT ''FromJSON)) nubbedTypes
   [d|
-    instance FromJSON (Some $(conT n)) where
+    instance $(pure constraints) => FromJSON (Some $(pure n')) where
       parseJSON v = do
         (tag', v') <- parseJSON v
-        $(caseE [|tag' :: String|] $ map (conMatchesParseJSON [|v'|]) cons ++ [wild])
+        $(caseE [|tag' :: String|] $ map pure matches ++ [wild])
     |]
-
--- | Generate all required matches (and some redundant ones...) for `eqTagged`
--- for some constructor
-conMatchesEqTagged :: Con -> [MatchQ]
-conMatchesEqTagged c = case c of
-    ForallC _ _ c' -> conMatchesEqTagged c'
-    GadtC _ tys _ -> forTypes (map snd tys)
-    _ -> error "conMatchesEqTagged: Unmatched constructor type"
-  where
-    name = conName c
-    forTypes ts =
-      [ do
-          as <- mapM (\_ -> newName "a") ts
-          bs <- mapM (\_ -> newName "b") ts
-          x <- newName "x"
-          y <- newName "y"
-          let compareTagFields = foldr (\(a, b) e -> [| $(varE a) == $(varE b) && $(e) |]) [| True |] (zip as bs)
-          match
-            (tupP [conP name (map varP as), conP name (map varP bs)])
-            (normalB (lamE [varP x, varP y] [| $(compareTagFields) && eq1 $(varE x) $(varE y) |] ))
-            []
-      , match
-          (tupP [conP name (map (const wildP) ts), wildP])
-          (normalB [| \ _ _ -> False |])
-          []
-      ]
 
 -- | Implementation of 'toJSON'
 conMatchesToJSON :: Con -> MatchQ
@@ -123,29 +119,46 @@ conMatchesToJSON c = do
 
 
 -- | Implementation of 'parseJSON'
-conMatchesParseJSON :: ExpQ -> Con -> MatchQ
+conMatchesParseJSON :: ExpQ -> Con -> WriterT [Type] Q Match
 conMatchesParseJSON e c = do
   let name = conName c
       match' = match (litP (StringL (nameBase name)))
   let forTypes types = do
         vars <- forM types $ \typ -> do
-          x <- newName "x"
+          x <- lift $ newName "x"
           case typ of
             AppT (ConT tn) (VarT vn) -> do
               -- This may be a nested GADT, so check for special FromJSON instance
-              idec <- reifyInstances ''FromJSON [AppT (ConT ''Some) (ConT tn)]
-              return $ case idec of
-                [] -> (VarP x, VarE x)
-                _ -> (ConP 'This [VarP x], VarE x) -- If a FromJSON instance is found for Some f, then we use it.
-            _ -> return (VarP x, VarE x)
+              idec <- lift $ reifyInstances ''FromJSON [AppT (ConT ''Some) (ConT tn)]
+              case idec of
+                [] -> do
+                  tell [typ]
+                  return (VarP x, VarE x)
+                _ -> return $ (ConP 'This [VarP x], VarE x) -- If a FromJSON instance is found for Some f, then we use it.
+            _ -> do
+              tell [typ]
+              return (VarP x, VarE x)
         let pat = return $ TupP (map fst vars)
             conApp = return $ foldl AppE (ConE name) (map snd vars)
             body = doE [ bindS pat [| parseJSON $e |]
                        , noBindS [| return (This $conApp) |]
                        ]
-        match' (normalB body) []
+        lift $ match' (normalB body) []
   case c of
     ForallC _ _ c' -> conMatchesParseJSON e c'
     GadtC _ tys _ -> forTypes (map snd tys)
     NormalC _ tys -> forTypes (map snd tys)
     _ -> error "conMatchesParseJSON: Unmatched constructor type"
+
+kindArity :: Kind -> Int
+kindArity = \case
+  ForallT _ _ t -> kindArity t
+  AppT (AppT ArrowT _) t -> 1 + kindArity t
+  SigT t _ -> kindArity t
+  ParensT t -> kindArity t
+  _ -> 0
+
+tyConArity :: Name -> Q Int
+tyConArity n = reify n >>= return . \case
+  TyConI (DataD _ _ ts mk _ _) -> fromMaybe 0 (fmap kindArity mk) + length ts
+  _ -> error $ "tyConArity: Supplied name reified to something other than a data declaration: " <> show n
