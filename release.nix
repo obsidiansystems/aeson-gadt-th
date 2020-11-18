c0abf31f9b354a28b9a5f840f0c256a6487f4034
Ali Abrar <aliabrar@gmail.com>
2020-11-18 17:17:06 -0500

    ci: add old compilers to nix ci

--- release.nix
+++ release.nix
@@ -1,8 +1,16 @@
-{ pkgs ? import (builtins.fetchTarball {
+{ }:
+let
+   nixos2003 = import (builtins.fetchTarball {
       name = "nixos-20.03_2020-11-18";
       url = "https://github.com/nixos/nixpkgs/archive/f05c380a51daee53ac2edb0bac2fd5f1774e977a.tar.gz";
       sha256 = "1xkgv4kvh2nii1kbxi0brjnb15scpzk8rkp7mzycgqh1lzfg23im";
-    }) {}
-}:
-let compilers = [ "ghc802" "ghc844" "ghc865" "ghc884" "ghc8102" ];
-in map (c: pkgs.haskell.packages."${c}".callCabal2nix "aeson-gadt-th" ./. {}) compilers
+    }) {};
+    nixos1903 = import (builtins.fetchTarball {
+      name = "nixos-19.03_2020-11-18";
+      url = "https://github.com/nixos/nixpkgs/archive/34c7eb7545d155cc5b6f499b23a7cb1c96ab4d59.tar.gz";
+      sha256 = "11z6ajj108fy2q5g8y4higlcaqncrbjm3dnv17pvif6avagw4mcb";
+    }) {};
+   oldCompilers = [ "ghc802" "ghc844" ];
+   compilers = [ "ghc865" "ghc884" "ghc8102" ];
+   compileWith = ghcs: pkgs: map (c: pkgs.haskell.packages."${c}".callCabal2nix "aeson-gadt-th" ./. {}) ghcs;
+in compileWith compilers nixos2003 ++ compileWith oldCompilers nixos1903
