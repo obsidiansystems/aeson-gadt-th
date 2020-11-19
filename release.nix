1b659ed6568d253c41d4243218515dad47a90657
Ali Abrar <aliabrar@gmail.com>
2020-11-18 19:42:01 -0500

    cabal: Fix version bounds

--- release.nix
+++ release.nix
@@ -1,14 +1,22 @@
 { }:
 let
   nixpkgsSets = import ./.ci/nixpkgs.nix;
-  inherit (nixpkgsSets) nixos2003 nixos1809;
+  inherit (nixpkgsSets) nixos1809 nixos2003;
   inherit (nixos2003) lib;
   ghcs = {
-    ghc802 = nixos1809;
-    ghc844 = nixos1809;
-    ghc865 = nixos2003;
-    # ghc884 = nixos2003;
-    # ghc8101 = nixos2003;
+    ghc802 = nixos1809.haskell.packages.ghc802;
+    ghc844 = nixos1809.haskell.packages.ghc844;
+    ghc865 = nixos2003.haskell.packages.ghc865;
+    ghc884 = nixos2003.haskell.packages.ghc884.override {
+      overrides = self: super: {
+        template-haskell = super.template-haskell_2_15_0_0;
+      };
+    };
+    ghc8101 = nixos2003.haskell.packages.ghc884.override {
+      overrides = self: super: {
+        template-haskell = super.template-haskell_2_15_0_0;
+      };
+    };
   };
 in 
-  lib.mapAttrs (ghc: pkgs: pkgs.haskell.packages."${ghc}".callCabal2nix "aeson-gadt-th" ./. {}) ghcs
+  lib.mapAttrs (_: ghc: ghc.callCabal2nix "aeson-gadt-th" ./. {}) ghcs
